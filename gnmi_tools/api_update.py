777ae5b6a28b0443c69721a3127c126b7532e992
Marcelo Reis <mareis@cisco.com>
2020-02-04 15:20:27 -0500

    Initial commit

--- gnmi_tools/api_update.py
+++ gnmi_tools/api_update.py
@@ -32,4 +32,4 @@ class GNMIManagerV2(gNMIManager):
             raise GNMIException(f'Unable to connect to "{self.host}:{self.port}"')
 
     def get_config(self, *args, **kwargs):
-        super().get_config(encoding=self.encoding, *args, **kwargs)
+        super().get_config(self.encoding, *args, **kwargs)
